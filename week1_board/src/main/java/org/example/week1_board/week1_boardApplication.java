af4976761b63e7f1cf0a23c0bb08ec2eadf9e737
joara <god0937@gmail.com>
2025-07-22 11:13:55 +0900

    25-07-22 게시판 초기 세팅

---
+++ week1_board/src/main/java/org/example/week1_board/week1_boardApplication.java
@@ -0,0 +1,13 @@
+package org.example.week1_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class week1_boardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(week1_boardApplication.class, args);
+    }
+
+}
---
+++ week1_board/src/test/java/org/example/week1_board/week1boardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.week1_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class week1boardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
